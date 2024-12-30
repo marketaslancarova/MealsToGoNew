4d5683a2b41f2848f1d8e9a8f704cf15f04ac655
marketaslancarova <marketaslancarova@gmail.com>
2024-12-30 20:16:11 +0100

    Add RestaurantsScreen with search functionality and mock data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,84 @@
-import { View, Text, StyleSheet } from "react-native";
+import {useState} from 'react';
+import { View, Text, StyleSheet,SafeAreaView,FlatList,Image,TextInput } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { mockRestaurants } from "../../data/restaurants";
+
 
 
 export default function RestaurantsScreen() {
+ 
+ 
   return (
-    <View style={styles.container}>
-      <Text>Restaurants</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Searchbar placeholder="Search here..."
+        value={""}
+        onChangeText={(text) => null}
+        onSubmitEditing={() => null} />
+        
+       <FlatList
+        data={mockRestaurants}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+          <View style={styles.info}>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text style={styles.address}>{item.address}</Text>
+              <Text style={styles.rating}>⭐ {item.rating}</Text>
+              <Text style={styles.status}>
+                {item.isOpenNow ? "Open Now" : "Closed"}
+              </Text>
+            </View>
+            </View>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f8f8f8",
+    padding: 16,
+  },
+  card: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    marginBottom: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  image: {
+    width: 80,
+    height: 80,
+  },
+  info:{
+    padding: 8,
+    flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+  },
+  name:{
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  address: {
+    color: "#666",
+    marginTop: 4,
+  },
+  rating: {
+    marginTop: 4,
+    color: "#f39c12",
+  },
+  status: {
+    marginTop: 4,
+    color: "#27ae60",
   },
 });
---
+++ data/restaurants.js
@@ -0,0 +1,98 @@
+export const mockRestaurants = [
+  {
+    name: "Zuni Café",
+    address: "1658 Market Street, San Francisco",
+    rating: 4.5,
+    isOpenNow: true,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+  },
+  {
+    name: "Blue Bottle Coffee",
+    address: "315 Linden St, San Francisco",
+    rating: 4.8,
+    isOpenNow: true,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+  {
+    name: "Chez Panisse",
+    address: "1517 Shattuck Ave, Berkeley",
+    rating: 4.7,
+    isOpenNow: false,
+    image:
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  },
+];
